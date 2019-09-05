1298514a65814a1a7726df1735d42fc2231350ea
Christopher Roller <chris.roller@centare.com>
2019-09-05 17:19:30 -0500

    Add project files.

---
+++ OnPaceRaceAdmin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OnPaceRaceAdmin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
